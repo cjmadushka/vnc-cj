832689975d8955efc0cc76b6119d5aacc45570a9
CJ Madushka <79196915+cjmadushka@users.noreply.github.com>
2021-02-23 19:00:39 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name = "remocolab.py",
+    version = "0.8",
+    py_modules = ['remocolab'],
+    url = "https://github.com/cjmadushka/vnc-cj",
+    author = "CJMadushka",
+)
